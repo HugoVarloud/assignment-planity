05a77fe3743aeab8ab451fa474262e05d0630146
hugo <hugo.varloud@gmail.com>
2024-09-25 07:39:09 +0200

    planity - feature - handle upload in component and general style

--- client/src/components/UploadButton/UploadButton.jsx
+++ client/src/components/UploadButton/UploadButton.jsx
@@ -1,44 +1,68 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useRef, useState } from 'react';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import CloseIcon from '@mui/icons-material/Close';
+import CheckIcon from '@mui/icons-material/Check';
 import axios from 'axios';
 import './UploadButton.css';
 
 const UploadButton = () => {
+  const inputRef = useRef();
   const [file, setFile] = useState(null);
-  const { loading, error } = useSelector((state) => state.file);
-  const dispatch = useDispatch();
+  const [progress, setProgress] = useState(0);
+  const [uploadStatus, setUploadStatus] = useState("select");
+
 
   const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
+    if (event.target.files && event.target.files.length > 0) {
+      setFile(event.target.files[0]);
+    }
   };
 
-  const uploadFile = async () => {
-    if (!file) {
-      alert('Please select a file!');
+  const onChooseFile = async () => {
+      inputRef.current.click();
+  };
+
+  const clearFileInput = () => {
+    inputRef.current.value = "";
+    setFile(null);
+    setProgress(0);
+    setUploadStatus("select");
+  };
+
+  const handleUpload = async () => {
+    if (uploadStatus === "done") {
+      clearFileInput();
       return;
     }
 
     try {
+      setUploadStatus("uploading");
+
       const formData = new FormData();
-      formData.append('file', file);
-      console.log('FORM DATA ', formData);
-      
-      // Make sure to handle the response properly
+      formData.append("file", file);
+
       const response = await axios.post(`${import.meta.env.VITE_API_PATH}/files/uploads`, formData, {
+        onUploadProgress: (progressEvent) => {
+          const percentCompleted = Math.round(
+            (progressEvent.loaded * 100) / progressEvent.total
+          );
+          setProgress(percentCompleted);
+        },
         responseType: 'blob',
       });
-      console.log("Response ", response);
 
       if (response && response.data) {
-        handleDownloadZip(response.data);  // Only pass the file data
+        handleDownloadZip(response.data);
       }
-    } catch (err) {
-      console.error("Error uploading file", err);
+
+      setUploadStatus("done");
+    } catch (error) {
+      setUploadStatus("select");
     }
-  };
+  }
 
   const handleDownloadZip = (zipBlob) => {
-    // Now zipBlob is just the binary data (response.data), not the entire response object
     const blob = new Blob([zipBlob], { type: 'application/zip' });
     const downloadUrl = URL.createObjectURL(blob);
     const link = document.createElement('a');
@@ -52,15 +76,50 @@ const UploadButton = () => {
 
   return (
     <>
-      <div className="container-upload-button">
-        <button onClick={uploadFile} disabled={loading}>
-          {loading ? 'Uploading...' : 'Upload and Download'}
-        </button>
-      </div>
       <div>
-        <input type="file" onChange={handleFileChange} />
+        <input
+          ref={inputRef}
+          type="file"
+          onChange={handleFileChange}
+          style={{ display: "none" }}
+        />
+        {!file && (
+          <button className="file-btn" onClick={onChooseFile}>
+            <FileUploadIcon/>Upload File
+          </button>
+        )}
+        {file && (
+          <>
+            <div className="file-card">
+              <UploadFileIcon/>
+              <div className="file-info">
+                <div style={{ flex: 1 }}>
+                  <h6>{file?.name}</h6>
+                  <div className="progress-bg">
+                    <div className="progress" style={{ width: `${progress}%` }} />
+                  </div>
+                </div>
+                {uploadStatus === "select" ? (
+                  <button onClick={clearFileInput}>
+                    <CloseIcon/>
+                  </button>
+                ) : (
+                  <div className="check-circle">
+                    {uploadStatus === "uploading" ? (
+                      `${progress}%`
+                    ) : uploadStatus === "done" ? (
+                      <CheckIcon/>
+                    ) : null}
+                  </div>
+                )}
+            </div>
+          </div>
+          <button className="upload-btn" onClick={handleUpload}>
+            {uploadStatus === "select" || uploadStatus === 'uploading' ? "Upload" : "Done"}
+          </button>
+          </>
+        )}
       </div>
-      {error && <div style={{ color: 'red' }}>{error}</div>}
     </>
   );
 };
